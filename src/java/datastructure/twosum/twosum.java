3612ef648f8028f460eee6939641985ab0a3ef0e
fullm <matoma1_@naver.com>
2024-03-26 14:48:42 +0900

    Initial commit

---
+++ src/java/datastructure/twosum/twosum.java
@@ -0,0 +1,49 @@
+package java.datastructure.twosum;
+
+public class twosum {
+   //target에게 인덱스 값을 리턴해야함.
+    //정확히 하나의 솔루션만 있다는걸 보증.
+    //같은 원소는 두 번 못쓴다.
+    //인덱스의 순서는 상관 없.
+    //하나씩 더해보기
+    //if target 10, nums= [1,2,3,4,5,6,7] -> 이걸 완전 탐색이라고
+    // 정답이 될 가능성이 있는 것을 모두 탐색
+   public static void main(String[] args) {
+
+   }
+
+}
+//반복문
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for( int i=0; i< nums.length; i++){
+            for( int j= i+1; j<=nums.length; j++){
+                if(nums[i]+ nums[j]== target){
+                    //return new int[]{i,j};
+                }
+            }
+        }
+        return new int[] {-1,-1}; //return null;
+        //조건문이 만족하지 않았을 때 리턴? 인트형 배열로 돌려줘야되고..뭐라냐
+    }
+}
+
+class Solution2{
+    public int[] threeSum(int[] nums, int target){
+        for( int i=0; i< nums.length; i++){
+            for(int j= i+1; j< nums.length; j++){
+                for(int k= j+1;k < nums.length; i++){
+                    if(nums[i] + nums[j]+ nums[k] == target){
+                        return new int[]{i,j,k};
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
+
+class Solution3{
+
+}
+
---
+++ src/java/required/part6/ch10_01_dfs.java
@@ -0,0 +1,77 @@
+package java.required.part6;
+
+import javax.security.sasl.SaslClient;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ch10_01_dfs {
+    static int[][] direction ={
+            //좌, 우, 상 ,하
+            {-1,0},{1,0},{0,-1},{0,1}
+    };
+    //단지의 크기.
+    static int size =0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.valueOf(sc.nextLine());
+        //지도는 N * N 사이즈를 가지고 있기 때문
+        int[][]map = new int[N][N];
+        //방문 여부 확인.
+        boolean[][] visited = new boolean[N][N];
+
+        //N크기만큼 지도를 입력받아 0,1로 지도를 채워줌
+        for( int i=0; i<N; i++){
+            String s = sc.nextLine();
+            for(int j=0; j < N; j++){
+                //하나씩 쪼개서 지도에 넣어줘야하는데, 그대로 지도에 넣게 되면 아스키 코드 값이 들어감
+                // -0을 해서 0,1 값으로 넣어줌
+                map[i][j] = s.charAt(j)-'0';
+            }
+        }
+        //input으로 지도 초기화 끝냄.
+
+        //
+        List<Integer> result = new ArrayList<>();
+
+        //이중배열, 이중포문
+        for(int i=0; i< N; i++){
+            for(int j=0; j< N; j++){
+                //map i,j에 값이 1이라는 것은 해당 위치에 집이 있다는 것
+               if(  map[i][j] == 1 && visited [i][j]){
+                   size = 1;
+                   //dfs 에 파라미터 넣어서 탐색해줌
+                   dfs(N, map, visited, i, j);
+                   //dsf탐색이 종료된 시점의 size 값을 result 에 넣어줌
+                   result.add(size);
+               }
+            }
+        }
+        System.out.println(result.size());
+        Collections.sort(result);
+        result.forEach(i -> System.out.println(i));
+    }
+    //dfs탐색에 필요한 지도의 크기 N, map의 데이터 맵, 방문 여부 visited, 현재 좌표값 x,y를 받는다
+    public static void dfs(int N, int [][]map, boolean[][]visited, int x, int y){
+        //현재 받은 x,y는 visited 체크 해 줌
+        visited[x][y] = true;
+        //인접한 상하좌우 확인
+        for( int i=0; i< direction.length ; i++){
+            //상하좌우 이동된 새로운 x, y좌료값 얻고
+            int nX = x + direction[i][0];
+            int nY = y + direction[i][1];
+        //이 좌표가 지도상에서 가능한 좌표인지 확인 해봐야 함. 0보다 크고 n보다 작아야 함
+            //이렇게 하면 인접한 집이 있고, 아직 방문하지 않은 집을 찾은 것.
+            if( nX >=0 && nY >= 0 && nX < N && nY < N && map[nY][nX] ==1 && !visited[nY][nX]){
+               //찾은 집을 +1 해줌
+                size +=1;
+                //새로 발견한 nx, ny기준으로 계속해서 영역 확장이 가능한지
+                //dfs 검색
+                //더 이상 연결 가능한 집이 없다면 종료
+                dfs(N, map, visited,nX, nY);
+            }
+        }
+    }
+}
